52584aef06991ab66801a0bb9b768d1d8f802081
gunt <4261629+gunt@users.noreply.github.com>
2020-06-30 16:42:16 +0200

    reviewing methods of API

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,50 @@
 const btnSearch = document.getElementById('btnSearch');
 const txtSearch = document.getElementById('domains');
 const resultArea = document.getElementById('result');
+// const discussion = disc;
 let out = "";
 
 btnSearch.onclick = function () {
-  var searchTerm = txtSearch.nodeValue;
-  const url = ''
+  var searchTerm = txtSearch.value;
+  const url = `reddit.com/r/webdev.json=${searchTerm}`;
+  console.log(url)
+  fetch(url, {
+      mode: 'no-cors'
+    }).then(function (data) {
+      return data.json();
+    })
+    .then(function (jsonObject) {
+      console.log(jsonObject);
+      for (disc in jsonObject) {
+        const discInfo = new Array(jsonObject[disc].selftext, jsonObject[disc].title, jsonObject[disc].images.source.url)
+        discOut(discInfo);
+      }
+      resultArea.innerHTML = out;
+    })
+    .catch(function (e) {
+      console.log("Error: + e");
+    });
 }
 
 
-document
-  .getElementById('fetchUserDataBtn')
-  .addEventListener('click', fetchUserData);
+function discOut(disc) {
+  console.log(disc);
+  out += `<div class="disc">
+  <div class="discImage"><img scr="${disc[2]}"/></div>
+  <div class="discText">
+  <h2>${disc[0]}</h2>
+  <h2>${disc[1]}</h2>
+  </div>
+  </div>`;
 
-function fetchUserData() {
-  fetch('https://pokeapi.co/api/v2/pokemon/?limit=50')
-    .then((response) => response.json())
-    .then((json) => console.log(json));
 }
+
+// document
+//   .getElementById('btnSearch')
+//   .addEventListener('click', fetchUserData);
+
+// function fetchUserData() {
+//   fetch('https://www.reddit.com/r/webdev.json')
+//     .then((response) => response.json())
+//     .then((json) => console.log(json));
+// }
