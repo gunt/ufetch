0cd0668627761eb36ec782ff1bc2b20c7abb741b
gunt <4261629+gunt@users.noreply.github.com>
2020-06-30 13:21:36 +0200

    const variables function

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,14 @@
+const btnSearch = document.getElementById('btnSearch');
+const txtSearch = document.getElementById('domains');
+const resultArea = document.getElementById('result');
+let out = "";
+
+btnSearch.onclick = function () {
+  var searchTerm = txtSearch.nodeValue;
+  const url = ''
+}
+
+
 document
   .getElementById('fetchUserDataBtn')
   .addEventListener('click', fetchUserData);
